5e436ea34573acc8a36da2d0acd6f963b6c0e3eb
ValipiReddy-web <72799893+ValipiReddy-web@users.noreply.github.com>
2025-09-01 19:56:39 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.9.0"
 
   required_providers {
     aws = {
@@ -8,15 +8,7 @@ terraform {
     }
   }
 
-#Shared storage for terraform state files
-  backend "s3" {
-    bucket         = "terraform-state-bucketsai-fresh"  #Get the state file from these here and update the state file from these here
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"  #this for state file locking
-    profile        = "terraformprofile"
-  }
-}
+ 
 
 # AWS Provider Configuration
 provider "aws" {
@@ -28,7 +20,7 @@ provider "aws" {
 
 # EC2 Module
 module "ec2_instance" {
-  source        = "./modules/ec2"
+  source        = "../../modules/ec2"
   instance_name = var.ec2_name
   ami_id        = var.ami_id
   instance_type = var.instance_type
@@ -38,13 +30,13 @@ module "ec2_instance" {
 
 # S3 Module
 module "s3_bucket" {
-  source      = "./modules/s3"
+  source      = "../../modules/s3"
   bucket_name = var.s3_bucket_name
 }
 
 # IAM User Module
 module "iam_user" {
-  source    = "./modules/iam"
+  source    = "../../modules/iam"
   user_name = var.iam_user_name
 }
 
